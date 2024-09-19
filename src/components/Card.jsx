9ecd0b2f279d9573a1925825dc30af573fa76f05
Zaid Arshad Sunasra <zaidsunasra26@gmail.com>
2024-09-19 19:24:30 +0530

    Added status icon to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,11 @@ import no from '../assets/No-priority.svg'
 import high from '../assets/Img - High Priority.svg'
 import medium from '../assets/Img - Medium Priority.svg'
 import urgent from '../assets/SVG - Urgent Priority grey.svg'
+import done from '../assets/Done.svg'
+import todo from '../assets/To-do.svg'
+import cancel from '../assets/Cancelled.svg'
+import progress from '../assets/in-progress.svg'
+import backlog from '../assets/Backlog.svg'
 
 const formatName = (name) => {
     if (!name) return 'Unknown';
@@ -15,6 +20,21 @@ const formatName = (name) => {
     return firstName.charAt(0).toUpperCase();
 };
 
+const getStatusIcon = (status) => {
+    switch (status) {
+        case "Todo":
+            return todo;
+        case "In progress":
+            return progress;
+        case "Backlog":
+            return backlog;
+        case "Done":
+            return done;
+        case "Cancelled":
+            return cancel;
+    }
+}
+
 export function Card({ ticket, name, group }) {
 
     const priorityArray = [no, low, medium, high, urgent];
@@ -24,11 +44,16 @@ export function Card({ ticket, name, group }) {
         <div className='card-header'>
             <div className='card-id'>{ticket.id}</div>
             {group == 'User' ? null : (
-                <div className='card-name' style={{backgroundColor: color[Math.floor(Math.random()* color.length)]}}>{formatName(name)}</div>
+                <div className='card-name' style={{ backgroundColor: color[Math.floor(Math.random() * color.length)] }}>{formatName(name)}</div>
             )}
-            
+
         </div>
         <div className='card-title'>
+            <div>
+                {group != "Status" ? (
+                    <img src={getStatusIcon(ticket.status)}></img>
+                ) : null}
+            </div>
             {ticket.title.slice(0, 69)}
             {ticket.title.length > 69 ? "..." : ""}
         </div>
